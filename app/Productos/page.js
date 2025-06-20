9461dc4060cf0301aa78b30e9a2057655ecb28c1
branding-emocion <pedrops@branding4x4.com>
2025-06-20 14:46:16 -0500

    server

--- app/Productos/page.js
+++ app/Productos/page.js
@@ -4,16 +4,7 @@ import { dbAdmin } from "@/firebase/firebaseAdmin";
 import GetProductos from "@/lib/GetProductos";
 import StoreComponent from "./StoreComponent";
 
-// export const revalidate = 86400; // revalidate at most every hour
-// export const metadata = {
-//   title: "Tienda REANDA",
-//   description:
-//     "Descubre en REANDA productos de alta calidad para costura y confección, diseñados para inspirar creatividad y sostenibilidad. Ofrecemos opciones para todos los niveles, con un servicio personalizado que garantiza una experiencia excepcional. Transformamos tus ideas en realidad, caminando juntos hacia un futuro creativo.",
-
-//   openGraph: {
-//     title: "Tienda REANDA",
-//   },
-// };
+export const revalidate = 86400; // revalidate at most every hour
 
 const Productos = async () => {
   try {
